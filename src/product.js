f590202246f56d1d65c28bd4e4c665345ec986ee
Heshan1478 <it23754652@my.sliit.lk>
2026-03-20 22:23:28 +0530

    refactor product.js

--- src/product.js
+++ src/product.js
@@ -1,13 +1,16 @@
 class Product {
   constructor(sku, name, price) {
-    if (!sku) throw new Error('SKU is required');
-    if (typeof price === 'undefined') throw new Error('Price is required');
-    if (price < 0) throw new Error('Price must be non-negative');
-    if (!name) throw new Error('Name is required');
+    this._validate(sku, name, price);
     this.sku = sku;
     this.name = name;
     this.price = price;
   }
-}
 
+  _validate(sku, name, price) {
+    if (!sku) throw new Error('SKU is required');
+    if (!name) throw new Error('Name is required');
+    if (typeof price === 'undefined') throw new Error('Price is required');
+    if (price < 0) throw new Error('Price must be non-negative');
+  }
+}
 module.exports = Product;
